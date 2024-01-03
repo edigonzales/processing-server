c78eedb1a83b757efa18c435fbe857b5c78ff0f8
edigonzales <edi.gonzales@gmail.com>
2024-01-03 21:41:31 +0100

    wip

--- processing-server/src/main/java/ch/so/agi/processing/rest/ProcessesApi.java
+++ processing-server/src/main/java/ch/so/agi/processing/rest/ProcessesApi.java
@@ -2,6 +2,8 @@ package ch.so.agi.processing.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 public class ProcessesApi {
     private Logger log = LoggerFactory.getLogger(this.getClass());
